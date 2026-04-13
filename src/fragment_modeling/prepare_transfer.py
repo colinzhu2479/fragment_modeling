06262c1c29dc51487c7f93a39e4b06c034a17c21
Xiao_iy22 <colinzhu2479@gmail.com>
2026-04-13 15:34:10 -0400

    clean up

--- src/fragment_modeling/prepare_transfer.py
+++ src/fragment_modeling/prepare_transfer.py
@@ -21,12 +21,8 @@ def load_force_input(frag_name, file_path):
 
 
     for n in range(len(atom_num)):
-        #dict_temp = dict({})
         for direction in ['x', 'y', 'z']:
             dis_list, force_t = get_force_io(xyz_a[n], atom_num[n], num_atom, force[n], direction)
-            #dict_temp[direction+'d'], dict_temp[direction+'f'] = dis_list, force_t
-            #train_dict[frag_name].append(((dict_temp['xd'], dict_temp['yd'], dict_temp['zd']),
-            #                          (dict_temp['xf'], dict_temp['yf'], dict_temp['zf']), e[n]))
             train_dict[direction+'d'].append(dis_list)
             train_dict[direction+'f'].append(force_t)
     return train_dict
@@ -45,7 +41,6 @@ def get_force_io(xyz_a, atom_num, num_atom, force, target_direction):
     ##                                                                         target_direction, ref)
     ##ref=np.array(xyz)
     ref = np.ones([num_atom, 3]) * 3
-    ''''''
 
     atomic_numbers, xyz = atom_num, xyz_a
     atomic_mass = fragment_transform.atom_mass_mapping(atomic_numbers)
@@ -55,18 +50,11 @@ def get_force_io(xyz_a, atom_num, num_atom, force, target_direction):
                                                                                     ref)
     #### simple sorting dis list
     dis_matrix = distance.cdist(xyz, xyz)
-    # if (n ==0 or n==1or n==2 or n==12) and False:
-    #   print(dis_matrix)
     k = 0
     for i in range(len(dis_matrix) - 1):
         for j in range(i, len(dis_matrix) - 1):
             dis_list[k] = dis_matrix[i][j + 1]
             k += 1
-    # v[0]=math.copysign(1,v[0,0])*v[0]
-    # v[1] = math.copysign(1, v[1, 1]) * v[1]
-    # v[2]=math.copysign(1,v[2,2])*v[2]
     force_t = np.array([np.matmul(v, force_t[iii]) for iii in range(num_atom)])[:,fragment_transform.direction_order(target_direction)]
 
-    # dis_list=np.delete(dis_list,np.where(energy==0),axis=0)
-    # energy = np.delete(energy,np.where(energy==0))
     return dis_list.tolist(), force_t.tolist()
